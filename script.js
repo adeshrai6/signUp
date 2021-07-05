93bb4211ed9f48bdc4ae2aab553a35c90c748501
Adesh <61568773+adeshrai6@users.noreply.github.com>
2021-07-04 23:33:27 -0400

    adding js file

---
+++ script.js
@@ -0,0 +1,20 @@
+const from = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+}
+
+from.addEventListener('submit', function (e) {
+  e.preventDefault();
+  if (username.value === '') {
+    showError(username, 'Username is required');
+  }
+  showSuccess(username);
+});
