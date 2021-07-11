641f01d14c4f6e2d4e974592d71acebfde98f123
Adesh <61568773+adeshrai6@users.noreply.github.com>
2021-07-10 20:39:45 -0400

    Authentication

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const from = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
-const password2 = document.getElementById('password confirmation');
+const password2 = document.getElementById('password Confermation');
 
 //Show input error message
 function showError(input, message) {
@@ -21,7 +21,18 @@ function showSuccess(input) {
 //Email validator
 function isValidEmail(email) {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (!re.test(input.value.trim())) {
+    showError(input, 'Email is not valid');
+  }
+  showSuccess(input);
   return re.test(String(email).toLowerCase());
+}
+
+//Check Password
+function checkPasswordMatch(firstInput, secondInput) {
+  if (firstInput.value !== secondInput.value) {
+    showError(secondInput, 'Password did not match');
+  }
 }
 
 //Check required fileds
@@ -35,12 +46,33 @@ function checkRequired(inputArr) {
   });
 }
 
+//Check length
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at leasr ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+}
+
+//Get Field Name
 function getFieldName(input) {
-  return input.id.charAt(0).toUppercase() + input.id.slice(1);
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
 //Event Listners
 from.addEventListener('submit', function (e) {
   e.preventDefault();
   checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkPasswordMatch(password, password2);
 });
